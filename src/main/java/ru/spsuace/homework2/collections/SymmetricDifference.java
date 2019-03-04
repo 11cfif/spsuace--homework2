9971890478de00643c389ab33b49e959f4e35998
Polina Shirobokova <izhevsk07@gmail.com>
2019-03-05 01:37:11 +0300

    Задание SymmetricDifference

--- src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
+++ src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
@@ -1,5 +1,6 @@
 package ru.spsuace.homework2.collections;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -7,11 +8,19 @@ import java.util.Set;
  * Реализуйте симметричную разность двух множеств: https://ru.wikipedia.org/wiki/Симметрическая_разность
  * Исходные множества модифицировать нельзя.
  * Пример: {1, 2, 3} и {0, 1, 2} -> {0, 3}.
- *
  */
 public class SymmetricDifference {
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> set3 = new HashSet<>();
+        Set<T> set4 = new HashSet<>();
+        set3.addAll(set1);
+        set3.addAll(set2);
+        set4.addAll(set1);
+        set4.addAll(set2);
+        set3.retainAll(set1);
+        set3.retainAll(set2);
+        set4.removeAll(set3);
+        return set4;
     }
 }
