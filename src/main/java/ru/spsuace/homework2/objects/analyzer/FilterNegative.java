877b6594bfafc84c70a68e2f4bd9ab8eac332fe6
Valeriy Smolskiy <smolskiy_pmi@mail.ru>
2019-03-06 22:11:18 +0300

    TextFilter кое-что изменил с классами

--- src/main/java/ru/spsuace/homework2/objects/analyzer/FilterNegative.java
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/FilterNegative.java
@@ -3,12 +3,11 @@ package ru.spsuace.homework2.objects.analyzer;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class FilterNegative extends StringTextAnalyze implements TextAnalyzer {
-    private final FilterType filter = FilterType.NEGATIVE_TEXT;
-    Collection<String> negative = Arrays.asList("=(", ":(", ":|");
+public class FilterNegative extends FilterSpam {
 
-    @Override
-    public FilterType analyzeText(String text) {
-        return analyzeTextString(text, filter, negative);
+
+    public FilterNegative() {
+        super(Arrays.asList("=(", ":(", ":|"), FilterType.NEGATIVE_TEXT);
     }
+
 }
--- src/main/java/ru/spsuace/homework2/objects/analyzer/FilterSpam.java
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/FilterSpam.java
@@ -2,17 +2,29 @@ package ru.spsuace.homework2.objects.analyzer;
 
 import java.util.Collection;
 
-public class FilterSpam extends StringTextAnalyze implements TextAnalyzer {
-    private final FilterType filter = FilterType.SPAM;
-    private Collection<String> spam;
+public class FilterSpam implements TextAnalyzer {
+    protected final FilterType filter;
+    protected Collection<String> str;
 
-    public FilterSpam(Collection<String> spam) {
-        this.spam = spam;
+
+    public FilterSpam(Collection<String> str) {
+        this.str = str;
+        filter = FilterType.SPAM;
+    }
+
+    public FilterSpam(Collection<String> str, FilterType filter) {
+        this.str = str;
+        this.filter = filter;
     }
 
 
     @Override
     public FilterType analyzeText(String text) {
-        return analyzeTextString(text, filter, spam);
+        for (String element : str) {
+            if (text.contains(element)) {
+                return filter;
+            }
+        }
+        return FilterType.GOOD;
     }
 }
