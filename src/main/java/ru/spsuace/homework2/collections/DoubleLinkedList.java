778018b2f8d7f0e85bfdb1cf4d22209e385c16e5
ELena <evseevalenoshka>
2019-03-10 23:17:43 +0300

    Двойной связный список

--- src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
+++ src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
@@ -10,46 +10,198 @@ import java.util.Iterator;
  */
 public class DoubleLinkedList<T> implements Iterable<T> {
 
+    private LinksAndData<T> first;
+    private LinksAndData<T> last;
+    private int count;
+
     public int size() {
-        return 0;
+
+        return count;
     }
 
     public boolean contains(Object o) {
+
+        LinksAndData<T> currentElement = first;
+
+        for (int i = 0; i < count; i++) {
+
+            if (currentElement.data.equals(o)) {
+
+                return true;
+            }
+            currentElement = currentElement.next;
+        }
+
         return false;
     }
 
     public void clear() {
 
+        first = null;
+        last = null;
+        count = 0;
     }
 
     public void add(int index, T element) {
 
+        if (index > count || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        LinksAndData<T> newElement = new LinksAndData(element);
+
+        if (first == null) {
+            first = newElement;
+            last = newElement;
+
+            count++;
+        } else {
+
+            if (index == count) {
+                addLast(element);
+
+            } else if (index == 0) {
+                addFirst(element);
+
+            } else {
+
+                LinksAndData<T> currentElement = findElement(index);
+
+                currentElement.previous.next = newElement;
+                newElement.previous = currentElement.previous;
+                currentElement.previous = newElement;
+                newElement.next = currentElement;
+
+                count++;
+            }
+        }
     }
 
     public void addLast(T element) {
 
-    }
+        LinksAndData<T> newElement = new LinksAndData(element);
 
+        if (last == null) {
+            first = newElement;
+            last = newElement;
+        } else {
+            last.next = newElement;
+            newElement.previous = last;
+            last = newElement;
+        }
+
+        count++;
+    }
 
     public void addFirst(T element) {
 
+        LinksAndData<T> newElement = new LinksAndData(element);
+
+        if (first == null) {
+            first = newElement;
+            last = newElement;
+        } else {
+            first.previous = newElement;
+            newElement.next = first;
+            first = newElement;
+        }
+
+        count++;
     }
 
-
     public T set(int index, T element) {
-        return null;
+
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        LinksAndData<T> currentElement = findElement(index);
+        T currentData = currentElement.data;
+        currentElement.data = element;
+
+        return currentData;
     }
 
     public T get(int index) {
-        return null;
+
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        LinksAndData<T> currentElement = findElement(index);
+
+        return currentElement.data;
     }
 
     public int indexOf(T o) {
+
+        LinksAndData<T> currentElement = first;
+
+        for (int i = 0; i < count; i++) {
+            if (currentElement.data.equals(o)) {
+                return i;
+            }
+            currentElement = currentElement.next;
+        }
+
         return -1;
     }
 
     public T remove(int index) {
-        return null;
+
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        LinksAndData<T> currentElement = findElement(index);
+        T currentData = currentElement.data;
+
+        if (index == count - 1) {
+            currentElement.previous.next = null;
+            last = currentElement.previous;
+        } else if (index == 0) {
+            currentElement.next.previous = null;
+            first = currentElement.next;
+        } else {
+            currentElement.previous.next = currentElement.next;
+            currentElement.next.previous = currentElement.previous;
+        }
+
+        count--;
+
+        return currentData;
+    }
+
+    private LinksAndData<T> findElement(int index) {
+
+        LinksAndData<T> currentElement;
+
+        if (index < (count / 2)) {
+            currentElement = first;
+
+            for (int i = 0; i < index; i++) {
+                currentElement = currentElement.next;
+            }
+        } else {
+            currentElement = last;
+
+            for (int i = count - 1; i > index; i--) {
+                currentElement = currentElement.previous;
+            }
+        }
+
+        return currentElement;
+    }
+
+    private class LinksAndData<T> {
+
+        private LinksAndData<T> previous;
+        private T data;
+        private LinksAndData<T> next;
+
+        LinksAndData(T data) {
+            this.data = data;
+        }
     }
 
     /**
