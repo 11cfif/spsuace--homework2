d8f90eb9e9d2c166cbd469d033d688e043e92d93
6Naci <Eruolmearn@yandex.ru>
2019-03-17 20:43:00 +0300

    DoubleLinkedList (without iterator) v1.0

--- src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
+++ src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
@@ -1,6 +1,7 @@
 package ru.spsuace.homework2.collections;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Реализовать двусвязный список, аналог LinkedList в java (то что я рассказывал на лекции)
@@ -10,47 +11,210 @@ import java.util.Iterator;
  */
 public class DoubleLinkedList<T> implements Iterable<T> {
 
+    private class Node<T> {
+        private Node next;
+        private Node prev;
+        private T value;
+
+        public Node(T value) {
+            next = null;
+            prev = null;
+            this.value = value;
+        }
+    }
+
+    private Node last;   //  top of stack
+    private Node first;     // bottom of stack
+    private int counter;
+
+    public DoubleLinkedList() {
+        last = null;
+        first = null;
+        counter = 0;
+    }
+
+    public DoubleLinkedList(T element) {
+        first.value = element;
+        last.value = element;
+        counter = 1;
+    }
+
     public int size() {
-        return 0;
+        return counter;
     }
 
     public boolean contains(Object o) {
+
+        if (indexOf((T) o) != -1) {
+            return true;
+        }
+
         return false;
     }
 
     public void clear() {
-
+        last = null;
+        first = null;
+        counter = 0;
     }
 
     public void add(int index, T element) {
 
+        if ((index < 0 || index > size())) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            addFirst(element);
+        } else if (index == counter) {
+            addLast(element);
+        } else {
+            Node<T> node = new Node<>(element);
+            Node temp = search(index);
+
+            node.next = temp;
+            node.prev = temp.prev;
+            temp.prev.next = node;
+            temp.prev = node;
+
+            counter++;
+        }
+
+
     }
 
     public void addLast(T element) {
+        Node<T> node = new Node<>(element);
+
+        if (counter == 0) {
+            first = node;
+            last = node;
+        } else {
+
+            node.prev = last;
+
+            last.next = node;
+
+            last = node;
+
+        }
+
+        counter++;
+    }
+
+    public void addFirst(T element) {
+        Node<T> node = new Node<>(element);
+
+        if (counter == 0) {
+
+            first = node;
+            last = node;
+
+        } else {
+
+            node.next = first;
+            first.prev = node;
+            first = node;
+
+        }
+
+        counter++;
 
     }
 
+    public Node search(int index) {
+        if ((index < 0 || index >= size())) {
+            throw new IndexOutOfBoundsException();
+        }
 
-    public void addFirst(T element) {
+        if (index < size() / 2) {
+            Node temp = first;
+            for (int i = 0; i < size() / 2; i++) {
+                if (i == index) {
 
+                    return temp;
+                }
+                temp = temp.next;
+            }
+        } else {
+            Node temp = last;
+            for (int i = size() - 1; i >= size() / 2; i--) {
+                if (i == index) {
+
+                    return temp;
+                }
+                temp = temp.prev;
+            }
+        }
+
+        return null;
     }
 
 
     public T set(int index, T element) {
-        return null;
+
+        Node temp = search(index);
+        T result = (T) temp.value;
+        temp.value = element;
+        return result;
+
     }
 
     public T get(int index) {
-        return null;
+
+        Node result = search(index);
+        return (T) result.value;
+
     }
 
     public int indexOf(T o) {
+
+        Node temp = first;
+
+        for (int i = 0; i < size(); i++) {
+            if (o.equals(temp.value)) {
+                return i;
+            }
+
+            temp = temp.next;
+
+        }
+
         return -1;
     }
 
     public T remove(int index) {
-        return null;
+
+        Node temp = search(index);
+
+        if (index == 0) {
+
+            first = temp.next;
+            first.prev = null;
+
+            counter--;
+            return (T) temp.value;
+        }
+        if (index == size() - 1) {
+
+            temp = last;
+            last = temp.prev;
+            last.next = null;
+
+            counter--;
+            return (T) temp.value;
+        }
+
+        Node previous = temp.prev;
+        Node next = temp.next;
+
+        previous.next = next;
+        next.prev = previous;
+
+        counter--;
+        return (T) temp.value;
     }
+
 
     /**
      * Дополнительное задание
