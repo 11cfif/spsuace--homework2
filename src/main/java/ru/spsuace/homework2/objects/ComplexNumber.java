83760e20665966ebbb9b068f0d6ccdc968cb616e
semen3325 <semen.ganzhara@gmail.com>
2019-09-25 23:20:04 +0300

    ClassWork 1

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,51 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+
+    private final int real;
+    private final int complex;
+
+    public ComplexNumber(int real, int complex) {
+        this.real = real;
+        this.complex = complex;
+    }
+
+    public int getReal() {
+        return real;
+    }
+
+    public int getComplex() {
+        return complex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ComplexNumber that = (ComplexNumber) o;
+        return real == that.real && complex == that.complex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Math.abs(this.real) + Math.abs(this.complex);
+    }
+
+    @Override
+    public String toString() {
+        if (this.complex < 0) {
+            return this.real + " - " + Math.abs(this.complex) + "i";
+        } else {
+            return this.real + " + " + this.complex + "i";
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new ComplexNumber(4, -4));
+    }
 }
