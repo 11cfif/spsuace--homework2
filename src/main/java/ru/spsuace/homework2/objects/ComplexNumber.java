212918c1f2c3a4bd0ec9074811c70dbad9a31ab8
bryuk <bryukhova-a@mail.ru>
2019-09-26 20:01:35 +0300

    ComplexNumber

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,36 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+    public double re;
+    public double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+        public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public ComplexNumber getReciprocal() {
+        double denominator = re * re + im * im;
+        if (denominator != 0) {
+            return new ComplexNumber(re / denominator, -im / denominator);
+        } else {
+            throw new ArithmeticException("denominator = 0");
+        }
+
+    }public double abs() {
+        return Math.sqrt(re * re + im * im);
+    }
+
+    @Override
+    public String toString() {
+        return "Re = " + re + ", Im = " + im;
+    }
 }
