77d9eeb146a5a1c59b1dd4cd94cf3004b5579cec
MarinaShakhova1946 <shak.marina15@gmail.com>
2019-09-29 20:45:03 +0300

    Задания семинара.

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,56 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+    private final double realPart;
+    private final double imaginaryPart;
+    private static final double EPS = 0.0001; // Точность вычислений
+
+    public ComplexNumber(double re, double im) {
+        realPart = re;
+        imaginaryPart = im;
+    }
+
+    public double getRe() {
+        return realPart;
+    }
+
+    public double getIm() {
+        return imaginaryPart;
+    }
+
+    public double abs() { //модуль комплексного числа
+        return Math.sqrt(realPart * realPart + imaginaryPart * imaginaryPart);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Double.doubleToLongBits(this.realPart) != Double.doubleToLongBits(other.realPart)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.imaginaryPart) != Double.doubleToLongBits(other.imaginaryPart)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        double result = 1;
+        result = prime * result + realPart;
+        result = prime * result + imaginaryPart;
+        return (int) result;
+    }
+
+    @Override
+    public String toString() {
+        return "Complex: " + realPart + "+ i*" + imaginaryPart;
+    }
 }
--- src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
+++ src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
@@ -13,7 +13,32 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
+        if (str == null) {
+            return null;
+        }
+        if (str.isEmpty()) {
+            return null;
+        }
+        char[] array = str.toCharArray();
+        int count = 1;
+        int max = 0;
+        char maxChar = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == array[i - 1]) {
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                    maxChar = array[i - 1];
+                }
+                count = 1;
+            }
+        }
+        if (count > max) {
+            max = count;
+            maxChar = array[array.length - 1];
+        }
+        return new Pair<>(maxChar, max);
     }
 
     public static class Pair<T, V> {
