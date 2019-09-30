8561c6cc0a42b8c9a4c43dd7d574042fe7a34ec9
HOME TASK 1 <lilpowerfulann@gmail.com>
2019-09-30 23:57:12 +0300

    забыла строчку перед конструктором

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -8,6 +8,7 @@ package ru.spsuace.homework2.objects;
 public class ComplexNumber {
     private final double real;
     private final double imaginary;
+
     public ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
