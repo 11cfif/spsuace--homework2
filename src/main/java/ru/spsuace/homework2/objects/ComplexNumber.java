bcde52d6a565824cf8c196ac959d45ab9aee05f6
Nataly9898 <natalya.afimchenko@mail.ru>
2019-09-30 17:41:52 +0300

    Afimchenko

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -6,4 +6,41 @@ package ru.spsuace.homework2.objects;
  * Плюс реализуйте любой метод (операцию) над комплексными числами (на ваш выбор)
  */
 public class ComplexNumber {
+    private final double supposed;
+    private final double real;
+
+    public ComplexNumber(double supposed, double real) {
+        this.supposed = supposed;
+        this.real = real;
+    }
+
+    public double getSupposed() {
+        return supposed;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "ComplexNumber{" +
+                "supposed=" + supposed +
+                ", real=" + real +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ComplexNumber that = (ComplexNumber) o;
+        return Double.compare(that.supposed, supposed) == 0 &&
+                Double.compare(that.real, real) == 0;
+    }
 }
