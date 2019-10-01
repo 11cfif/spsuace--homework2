7a9159b90e1c5ccf123c833b27fac8d89c0b54bb
Iliya <boitsov-iliya@yandex.ru>
2019-10-01 23:41:01 +0300

    не знаю, что тут не так с форматированием. Надеюсь, так лучше

--- src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
+++ src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
@@ -13,21 +13,27 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
+
         if (str == null || str.isEmpty()) {
             return null;
         }
+
         char[] strChars = str.toCharArray();
         char maxRepeatingChar = strChars[0];
         int maxNumberOfRepeats = 1;
         int currentNumberOfRepeats = 1;
+
         for (int i = 1; i < strChars.length; i++) {
+
             if (strChars[i] == strChars[i-1]) {
                 currentNumberOfRepeats++;
             }
+
             if (currentNumberOfRepeats > maxNumberOfRepeats) {
                 maxNumberOfRepeats = currentNumberOfRepeats;
                 maxRepeatingChar = strChars[i];
             }
+
             if (strChars[i] != strChars[i-1]) {
                 currentNumberOfRepeats = 1;
             }
