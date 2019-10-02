989aee12c5077ef2873a295e0df746bf7a69e515
MarinaShakhova1946 <shak.marina15@gmail.com>
2019-10-02 17:28:02 +0300

    Задания версия 4.0 , аллилуйя все тесты прошли

--- src/main/java/ru/spsuace/homework2/objects/analyzer/TextAnalyzer.java
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/TextAnalyzer.java
@@ -1,6 +1,5 @@
 package ru.spsuace.homework2.objects.analyzer;
 
-
 /**
  * Базовый интерефейс фильтра, наследники этого интерефейса должны инкапсулировать в себе всю логику
  * анализа текста.
@@ -8,27 +7,76 @@ package ru.spsuace.homework2.objects.analyzer;
  * прием ООП, где нам не важна конкретная реализация, а важен только контракт, что результат статических методов
  * умеет как-то анализировать текст). Сами статические методы мне нужны для создания тестов,
  * что бы без реальных классов (которые вы напишите) я смог "сэмулировать" их создание.
- *
+ * <p>
  * Так же необходимо создать все необходимы методы, которые будут вам нужны для прогона текста
  * по всем фильтрам в классе TextFilterManager
  */
 
 public interface TextAnalyzer {
+    public class FilterLong implements TextAnalyzer {
+
+        private long maxLength;
+
+        FilterLong(long maxLength) {
+            this.maxLength = maxLength;
+        }
+
+        @Override
+        public FilterType doFilter(String str) {
+            if (str.length() > maxLength) {
+                return FilterType.TOO_LONG;
+            }
+            return FilterType.GOOD;
+        }
+    }
 
     static TextAnalyzer createTooLongAnalyzer(long maxLength) {
+        return new FilterLong(maxLength);
+    }
 
-        return null;
+    public class FilterSpam implements TextAnalyzer {
+
+        private String[] spamWord;
+
+        public FilterSpam(String[] spamWord) {
+            this.spamWord = spamWord;
+        }
+
+        @Override
+        public FilterType doFilter(String str) {
+            for (String word : spamWord) {
+                if (str.contains(word)) {
+                    return FilterType.SPAM;
+                }
+            }
+            return FilterType.GOOD;
+        }
     }
 
     static TextAnalyzer createSpamAnalyzer(String[] spam) {
+        return new FilterSpam(spam);
+    }
 
-        return null;
+    public class FilterNegative implements TextAnalyzer {
+
+        String emotions[] = {"=(", ":(", ":|"};
+
+        @Override
+        public FilterType doFilter(String str) {
+            for (String smile : emotions) {
+                if (str.contains(smile)) {
+                    return FilterType.NEGATIVE_TEXT;
+                }
+            }
+            return FilterType.GOOD;
+        }
     }
 
     static TextAnalyzer createNegativeTextAnalyzer() {
-
-        return null;
+        return new FilterNegative();
     }
+
+    public FilterType doFilter(String str);
 
     /**
      * Дополнительное задание: придумать свой фильтр
--- src/main/java/ru/spsuace/homework2/objects/analyzer/TextFilterManager.java
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/TextFilterManager.java
@@ -1,6 +1,5 @@
 package ru.spsuace.homework2.objects.analyzer;
 
-
 /**
  * Задание написать систему фильтрации комментариев.
  * Надо реализовать три типа обязательных фильтров
@@ -16,7 +15,9 @@ package ru.spsuace.homework2.objects.analyzer;
  * + в качестве доп задания, можно всем типам фильтров задать приоритет
  * (SPAM, TOO_LONG, NEGATIVE_TEXT, CUSTOM - в таком порядке) и возвращать тип с максимальным приоритетом.
  */
+
 public class TextFilterManager {
+    private TextAnalyzer[] filters;
 
     /**
      * Для работы с каждым элементом массива, нужно использовать цикл for-each
@@ -24,13 +25,26 @@ public class TextFilterManager {
      * что в них реализован интерфейс TextAnalyzer
      */
     public TextFilterManager(TextAnalyzer[] filters) {
-
+        this.filters = filters;
     }
 
     /**
      * Если переменная текст никуда не ссылается, то это означает, что не один фильтр не сработал
      */
     public FilterType analyze(String text) {
-        return null;
+        if (text == null || text.isEmpty()) {
+            return FilterType.GOOD;
+        }
+
+        FilterType filterType;
+
+        for (TextAnalyzer currentFilter : filters) {
+
+            filterType = currentFilter.doFilter(text);
+            if (filterType != FilterType.GOOD) {
+                return filterType;
+            }
+        }
+        return FilterType.GOOD;
     }
 }
