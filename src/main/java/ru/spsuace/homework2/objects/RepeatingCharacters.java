32427e3438c41caf73b7e632b0fd48dc6ab5dc6e
Анна Устюгова <C:\Users\\AppData\Roaming\The Bat!>
2019-10-06 13:01:29 +0300

    Complete repeating characters

--- src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
+++ src/main/java/ru/spsuace/homework2/objects/RepeatingCharacters.java
@@ -8,13 +8,34 @@ import java.util.Objects;
  * Если более одного символа с максимальным значением, то нужно вернуть тот символ,
  * который первый встречается в строчке
  * Если строка пустая или null, то вернуть null
- * Пример abbasbdlbdbfklsssbb -> (s, 4)
+ * Пример abbasbdlbdbfklsssbb -> (s, 3)
  */
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
-    }
+        if (str == null || str == "") {
+            return null;
+        }
+        char charMax = str.charAt(0);
+        int maxRepeatingCharacters = 0;
+        int repeatingCharacterCounter = 1;
+        int length = str.length();
+        for (int i = 0; i < length - 1; i++) {
+            char previousSymbol = str.charAt(i);
+            char nextSymbol = str.charAt(i + 1);
+            if (previousSymbol == nextSymbol) {
+                repeatingCharacterCounter += 1;
+            }
+            if (repeatingCharacterCounter > maxRepeatingCharacters){
+                maxRepeatingCharacters = repeatingCharacterCounter;
+                charMax = previousSymbol;
+            }
+            if (previousSymbol != nextSymbol) {
+                repeatingCharacterCounter = 1;
+            }
+        }
+            return new Pair<>(charMax, maxRepeatingCharacters);
+        }
 
     public static class Pair<T, V> {
         private final T first;
@@ -23,6 +44,10 @@ public class RepeatingCharacters {
         public Pair(T first, V second) {
             this.first = first;
             this.second = second;
+        }
+
+        public String toString() {
+            return " " + getFirst() + ',' + getSecond();
         }
 
         public T getFirst() {
