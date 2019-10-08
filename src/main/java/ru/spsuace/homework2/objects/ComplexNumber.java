8d5e983fb32dfec59856c2da47ec6471fc7a4800
luckyanya <anya.lakeeva1@mail.ru>
2019-10-08 14:29:21 +0300

    complex

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,35 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+
+    private final double real;
+    private final double im;
+
+    public ComplexNumber(double real, double im) {
+        this.real=real;
+        this.im=im;
+    }
+
+    public ComplexNumber(ComplexNumber z) {
+
+        this(z.real, z.im);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public ComplexNumber getZ() {
+        return new ComplexNumber(real,im);
+    }
+
+    @Override
+
+    public String toString() {
+            return "Комплексное число: " + real + " " + im;
+    }
 }
