f067fba644ae5e15bb085148aa8c58a970c1bfe2
Анна Устюгова <C:\Users\\AppData\Roaming\The Bat!>
2019-10-09 00:00:46 +0300

    partially complete StringTasks

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -1,5 +1,7 @@
 package ru.spsuace.homework2.objects.october2;
 
+import com.sun.tools.javac.util.StringUtils;
+
 public class StringTasks {
 
     /**
@@ -10,9 +12,24 @@ public class StringTasks {
      * Можно использовать функции Double.valueOf() и другие такие же
      */
     public static Number simpleValueOf(String str) {
-        return null;
-    }
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        int countDots = str.length() - str.replace(".", "").length();
+        if ((countDots) > 1 || (str.length() - str.replace("e", "").length()) > 1) {
+            return null;
+        }
 
+        str = str.replaceAll("(?<!^)[\\-]+", "");
+        str = str.replaceAll("[^0-9e\\-.]+", "");
+
+
+        if (countDots > 0) {
+            return Double.valueOf(str);
+        }
+        return Integer.valueOf(str);
+
+    }
 
     /**
      * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf()
