9c0e9efecef28df3c914edbfd7a404f61319129d
sanekels661 <elsakov982@inbox.ru>
2019-10-09 12:34:38 +0300

    MaxTask доделан и простой ValueOf

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -70,6 +70,52 @@ public class StringTasks {
      * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf()
      */
     public static Number valueOf(String str) {
+
+        if (str == null || str == "") {
+            return null;
+        }
+        char[] strCopy = new char[str.length()];
+        char[] strAnswerCopy = new char[str.length()];
+        String strAnswer = "";
+        char[] allowedSymbols = {'e', '.', '-'};
+        int[] ammountOfAllowedSybols = new int[3];
+
+        for (int i = 0; i < str.length(); i++) {
+            strCopy[i] = str.charAt(i);
+        }
+        for (char element : strCopy) {
+            for (int i = 0; i < 3; i++) {
+                if (element == allowedSymbols[i]) {
+                    ammountOfAllowedSybols[i]++;
+                }
+            }
+            if (((ammountOfAllowedSybols[2] > 1) && (ammountOfAllowedSybols[0] == 0)) ||
+                    ((ammountOfAllowedSybols[2] > 2) && (ammountOfAllowedSybols[0] == 1))) {
+                return null;
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            if (ammountOfAllowedSybols[i] > 1) {
+                return null;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            if (!(strCopy[i] >= '0' && strCopy[i] <= '9')) {
+                for (char element : allowedSymbols) {
+                    if (element == strCopy[i]) {
+                        strAnswer += str.charAt(i);
+                    }
+                }
+            } else {
+                str.charAt(i);
+                strAnswer += str.charAt(i);
+            }
+        }
+
+
+
+
         return null;
     }
 }
