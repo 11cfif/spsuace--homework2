b939dd2501c9c73e1e3d897968d472e7a7997fb5
Вероничка <nikki-vlad.mail@mail.ru>
2019-10-09 10:21:35 +0300

    ComplexNumber

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -6,4 +6,48 @@ package ru.spsuace.homework2.objects;
  * Плюс реализуйте любой метод (операцию) над комплексными числами (на ваш выбор)
  */
 public class ComplexNumber {
+    private final double real;
+    private final double imagine;
+
+    public ComplexNumber(double real, double imagine) {
+        this.real = real;
+        this.imagine = imagine;
+    }
+
+    public double getReal() {
+        return real;
+    }
+    public double getImagine() {
+        return imagine;
+    }
+    public double abs() {
+        return Math.sqrt(real * real + imagine * imagine);
+    }
+    @Override
+    public String toString() {
+        char sign;
+        if (imagine > 0) {
+            sign = '+';
+        } else {
+            sign = '-';
+        }
+        return "" + real + sign + Math.abs(imagine) + "i";
+    }
+    @Override
+    public int hashCode(){
+        final int prime=11;
+        double result=1;
+        result=prime*result+real;
+        result=prime*result+imagine;
+        return (int) result;
+    }
+    @Override
+    public boolean equals(Object n){
+        if (n==null){
+            return false;
+        } else{
+            ComplexNumber numb=(ComplexNumber)n;
+            return (Math.abs(this.real-numb.real)==10e-5 && Math.abs(this.imagine- numb.imagine)==10e-5);
+        }
+    }
 }
