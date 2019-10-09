6ccbcea5cb19d584921bf57b3156a279e20d0a9a
Anna <anya.lakeeva1@mail.ru>
2019-10-09 22:52:55 +0300

    string simple

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -8,11 +8,30 @@ public class StringTasks {
      * Если '.' и 'e' больше чем 1, возвращаем null
      * Любой класс-обертка над примитивами наследуется от Number
      * Можно использовать функции Double.valueOf() и другие такие же.
-     *
+     * <p>
      * Работайте со строкой, НЕ надо ее переводить в массив байт (это можно использовать только для цикла)
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number simpleValueOf(String str) {
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '-' || Character.isDigit(str.charAt(i)) || str.charAt(i) == 'e' || str.charAt(i) == '.') {
+                result += str.charAt(i);
+            }
+        }
+        if (result.matches("-?[0-9]+")) {
+            if (Long.valueOf(result) > Integer.MAX_VALUE || Long.valueOf(result) < Integer.MIN_VALUE) {
+                return Long.valueOf(result);
+            }
+            return Integer.valueOf(result);
+        }
+        if (result.matches("-?[0-9]+e-?[0-9]+") || result.matches("-?[0-9]+\\.[0-9]+e-?[0-9]+") || result.matches("-?[0-9]+\\.[0-9]")) {
+            return Double.valueOf(result);
+        }
         return null;
     }
 
