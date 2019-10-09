37b121de890d6faf6d47e2d691d7b10575452b85
sanekels661 <elsakov982@inbox.ru>
2019-10-09 13:57:51 +0300

    Merge branch 'master' of https://github.com/11cfif/spsuace--homework2 into HomeTask2
     Conflicts:
    	src/test/java/ru/spsuace/homework2/objects/october2/MaxTaskTest.java
    	src/test/java/ru/spsuace/homework2/objects/october2/StringTasksTest.java

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -91,27 +91,34 @@ public class StringTasks {
             return null;
         }
         char[] strCopy = new char[str.length()];
-        char[] strAnswerCopy = new char[str.length()];
         String strAnswer = "";
         char[] allowedSymbols = {'e', '.', '-'};
-        int[] ammountOfAllowedSybols = new int[3];
-
+        int[] ammountOfAllowedSymbols = new int[3];
         for (int i = 0; i < str.length(); i++) {
             strCopy[i] = str.charAt(i);
+        }
+        for (int i = 1; i < str.length(); i++) {
+            if (strCopy[i-1] == allowedSymbols[2] && strCopy[i] == allowedSymbols[0]){
+                return null;
+            }
         }
         for (char element : strCopy) {
             for (int i = 0; i < 3; i++) {
                 if (element == allowedSymbols[i]) {
-                    ammountOfAllowedSybols[i]++;
+                    ammountOfAllowedSymbols[i]++;
                 }
             }
-            if (((ammountOfAllowedSybols[2] > 1) && (ammountOfAllowedSybols[0] == 0)) ||
-                    ((ammountOfAllowedSybols[2] > 2) && (ammountOfAllowedSybols[0] == 1))) {
+            if (((ammountOfAllowedSymbols[2] > 1) && (ammountOfAllowedSymbols[0] == 0)) ||
+                    ((ammountOfAllowedSymbols[2] > 2) && (ammountOfAllowedSymbols[0] == 1))) {
+                return null;
+            }
+            if ((ammountOfAllowedSymbols[2] > 1) && (ammountOfAllowedSymbols[0] == 1) &&
+                    (str.indexOf(allowedSymbols[0]) < str.indexOf(allowedSymbols[2]))) {
                 return null;
             }
         }
         for (int i = 0; i < 2; i++) {
-            if (ammountOfAllowedSybols[i] > 1) {
+            if (ammountOfAllowedSymbols[i] > 1) {
                 return null;
             }
         }
@@ -128,10 +135,53 @@ public class StringTasks {
                 strAnswer += str.charAt(i);
             }
         }
+        if (strAnswer.charAt(strAnswer.length() - 1) == '-') {
+            return null;
+        }
+//        if ((ammountOfAllowedSymbols[0] > 0) || (ammountOfAllowedSymbols[1] > 0)) {
+//            return Double.valueOf(strAnswer);
+//        }
+//        if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
+//            return Long.valueOf(strAnswer);
+//        }
+//        return Integer.valueOf(strAnswer);
+
+        //private static getDigit(char str1){ return }
+        long answerLong = 0;
+
+        double answerDouble = 0;
+
+//        if (strAnswer.indexOf('e') != -1) {
+//            String[] strExp = strAnswer.split("e");
+//
+//        }
+        if (strAnswer.charAt(0) == '-'){
+
+            if (strAnswer.indexOf('e') != -1){
 
 
 
+                for (int i = 1; i < strAnswer.length(); i++){
+                    answerLong = (int)Math.pow(10,strAnswer.length()-i) *
+                            Character.digit(strAnswer.charAt(i),10);
+                }
+                answerLong *= -1;
+            }
+        }
 
+
+        for (int i = 0; i < strAnswer.length(); i++){
+            answerLong = (int)Math.pow(10,strAnswer.length()-i) *
+                    Character.digit(strAnswer.charAt(i),10);
+        }
+        if (answerLong < Integer.MAX_VALUE || answerLong > Integer.MIN_VALUE){
+            return (int)answerLong;
+        }
         return null;
+
     }
+
+   // private static int getDigit(char str1){ return Character.digit(str1, 10); }
+
+
 }
