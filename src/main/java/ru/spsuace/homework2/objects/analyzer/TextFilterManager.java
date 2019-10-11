3f1371c10e56bdf4ec97a8f0947135f572a5387f
Iliya <boitsov-iliya@yandex.ru>
2019-10-11 18:14:28 +0300

    "this." на всякий случай

--- src/main/java/ru/spsuace/homework2/objects/analyzer/TextFilterManager.java
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/TextFilterManager.java
@@ -41,7 +41,7 @@ public class TextFilterManager {
 
     public TextFilterManager(TextAnalyzer[] filters) {
         this.filters = filters;
-        Arrays.sort(filters, (filter1, filter2) -> {
+        Arrays.sort(this.filters, (filter1, filter2) -> {
             if (filter1.getPriority() < filter2.getPriority()) {
                 return -1;
             } else if (filter1.getPriority() == filter2.getPriority()) {
