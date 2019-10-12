9a57a36eb7768671c37044b3b6bf4c34b89ab1c5
Александр <elsakov982@inbox.ru>
2019-10-12 21:43:58 +0300

    перелапатил simpleValueOf, сделал с regex

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -15,64 +15,32 @@ public class StringTasks {
      * Работайте со строкой, НЕ надо ее переводить в массив байт (это можно использовать только для цикла)
      * У класса Character есть полезные методы, например Character.isDigit()
      */
+
     public static Number simpleValueOf(String str) {
 
         if (str == null || str == "") {
             return null;
         }
-
-        char[] strCopy = new char[str.length()];
-        char[] strAnswerCopy = new char[str.length()];
+        String[] strCopy = new String[str.length()];
         String strAnswer = "";
-        char[] allowedSymbols = {'e', '.', '-'};
+        String[] allowedSymbols = {"e", ".", "-"};
         int[] ammountOfAllowedSybols = new int[3];
 
         for (int i = 0; i < str.length(); i++) {
-            strCopy[i] = str.charAt(i);
+            strCopy[i] = String.valueOf(str.charAt(i));
         }
-        for (int i = 1; i < str.length(); i++) {
-            if (strCopy[i-1] == allowedSymbols[2] && strCopy[i] == allowedSymbols[0]){
-                return null;
-            }
-        }
-        for (char element : strCopy) {
-            for (int i = 0; i < 3; i++) {
-                if (element == allowedSymbols[i]) {
-                    ammountOfAllowedSybols[i]++;
-                }
-            }
-            if (((ammountOfAllowedSybols[2] > 1) && (ammountOfAllowedSybols[0] == 0)) ||
-                    ((ammountOfAllowedSybols[2] > 2) && (ammountOfAllowedSybols[0] == 1))) {
-                return null;
-            }
-            if ((ammountOfAllowedSybols[2] > 1) && (ammountOfAllowedSybols[0] == 1) &&
-                    (str.indexOf(allowedSymbols[0]) < str.indexOf(allowedSymbols[2]))) {
-                return null;
-            }
-        }
-        for (int i = 0; i < 2; i++) {
-            if (ammountOfAllowedSybols[i] > 1) {
-                return null;
-            }
-        }
-        for (int i = 0; i < str.length(); i++) {
 
-            if (!(strCopy[i] >= '0' && strCopy[i] <= '9')) {
-                for (char element : allowedSymbols) {
-                    if (element == strCopy[i]) {
-                        strAnswer += str.charAt(i);
-                    }
-                }
-            } else {
-                str.charAt(i);
+        for (int i = 0; i < str.length(); i++) {
+            if (strCopy[i].matches("[-e.]*[0-9]*")) {
                 strAnswer += str.charAt(i);
             }
         }
-        if (strAnswer.charAt(strAnswer.length() - 1) == '-') {
+        if (!strAnswer.matches("-?[0-9]+")){
+            if (strAnswer.matches("-?[0-9]+\\.[0-9]+") ||
+                    strAnswer.matches("-?([0-9]+\\.[0-9]+|[0-9]+)e-?[0-9]+")){
+                return Double.valueOf(strAnswer);
+            }
             return null;
-        }
-        if ((ammountOfAllowedSybols[0] > 0) || (ammountOfAllowedSybols[1] > 0)) {
-            return Double.valueOf(strAnswer);
         }
         if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
             return Long.valueOf(strAnswer);
