c1f9e43daf2281d3dc6df6c08d90ffbf14fb48e7
Nataly9898 <natalya.afimchenko@mail.ru>
2019-10-14 14:54:36 +0300

    Homework2

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -10,33 +10,7 @@ public class StringTasks {
      * Можно использовать функции Double.valueOf() и другие такие же
      */
     public static Number simpleValueOf(String str) {
-        if (str.isEmpty() || str == null) {
-            return null;
-        }
-        int valT = 0;
-        String res = "";
-        int i;
-        for (i = 0; i < str.length(); i++) {
-            if (Character.isDigit(str.charAt(i)) || str.charAt(i) == '.' || str.charAt(i) == 'e' || str.charAt(i) == '-') {
-                res = res + str.charAt(i);
-            }
-        }
-        if (res.matches("-?[0-9]+")) {
-            if (Long.valueOf(res) > Integer.MAX_VALUE || Long.valueOf(res) < Integer.MIN_VALUE) {
-                return Long.valueOf(res);
-            }
-            return Integer.valueOf(res);
-        }
-        if (res.matches("-?[0-9]+\\.[0-9]")) {
-            return Double.valueOf(res);
-        }
-        if (res.matches("-?[0-9]+e-?[0-9]+")) {
-            return Double.valueOf(res);
-        }
-        if (res.matches("-?[0-9]+\\.[0-9]+e-?[0-9]+")) {
-            return Double.valueOf(res);
-        }
-
+       
         return null;
     }
 
