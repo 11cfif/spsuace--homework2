f98b69317754303c3123b0af8d751ddf511998aa
Александр <elsakov982@inbox.ru>
2019-10-14 22:10:06 +0300

    сделал доп задание в stringTask (сразу в поервой функции simpleValueOf)

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -1,7 +1,5 @@
 package ru.spsuace.homework2.objects.october2;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 public class StringTasks {
 
     /**
@@ -23,6 +21,117 @@ public class StringTasks {
         }
         String[] strCopy = new String[str.length()];
         String strAnswer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            strCopy[i] = String.valueOf(str.charAt(i));
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (strCopy[i].matches("[-e.]*[0-9]*")) {
+                strAnswer += str.charAt(i);
+            }
+        }
+        String[] current = signAndString(strAnswer);
+        if (!strAnswer.matches("-?[0-9]+")) {
+            if (strAnswer.matches("-?[0-9]+\\.[0-9]+") ||
+                    strAnswer.matches("-?([0-9]+\\.[0-9]+|[0-9]+)e-?[0-9]+")) {
+                return strToDouble(strAnswer);
+            }
+            return null;
+        }
+        if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
+            return StrToLong(current[1], current[0]);
+        }
+        return strToInt(current[1], current[0]);
+    }
+
+    private static int strToInt(String str1, String strSign) {
+        int answer = 0;
+        int sign = 1;
+        if (strSign.charAt(0) == '-') {
+            sign = -1;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            answer += (int) Math.pow(10, str1.length() - i - 1) * Character.digit(str1.charAt(i), 10);
+        }
+        return sign * answer;
+    }
+
+    private static long StrToLong(String str1, String strSign) {
+        long answer = 0;
+        long sign = 1;
+        if (strSign.charAt(0) == '-') {
+            sign = -1;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            answer += (long) Math.pow(10, str1.length() - i - 1) * Character.digit(str1.charAt(i), 10);
+        }
+        return sign * answer;
+    }
+
+    private static String[] signAndString(String str1) {
+        String[] str2 = new String[2];
+        str2[0] = "+";
+        str2[1] = str1;
+        if (str1.charAt(0) == '-') {
+            str2 = str1.split("-", 2);
+            str2[0] = "-";
+        }
+        return str2;
+    }
+
+    private static String[] intFractParts(String str1) {
+        String[] str2 = new String[2];
+        str2[0] = str1;
+        str2[1] = "0";
+        if (str1.matches("[0-9]*\\.[0-9]*")) {
+            str2 = str1.split("\\.");
+        }
+        return str2;
+    }
+
+    private static String[] expPart(String str1) {
+        double answer = 0;
+        String[] str2 = new String[2];
+        str2[0] = str1;
+        str2[1] = "0";
+        if (str1.matches("[0-9]*.?[0-9]*e-?[0-9]*")) {
+            str2 = str1.split("e");
+        }
+        return str2;
+    }
+
+    private static double expPow(String str1) {
+        String[] str2 = new String[2];
+        str2 = signAndString(str1);
+
+        return Math.pow(10, strToInt(str2[1], str2[0]));
+    }
+
+    private static double strToDouble(String str1) {
+        String[] minusTest = new String[2];
+        minusTest = signAndString(str1);
+        String[] dotTest;
+        String[] expTest;
+        expTest = expPart(minusTest[1]);
+        dotTest = intFractParts(expTest[0]);
+        double beforeDot = strToInt(dotTest[0], minusTest[0]);
+        double afterDot = strToInt(dotTest[1], "+") * Math.pow(10, -dotTest[1].length());
+        double afterExp = expPow(expTest[1]);
+        return (beforeDot + afterDot) * afterExp;
+    }
+
+    /**
+     * Дополнительно задание
+     * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf() и new Integer и тд
+     */
+    public static Number valueOf(String str) {
+
+        if (str == null || str == "") {
+            return null;
+        }
+        String[] strCopy = new String[str.length()];
+        String strAnswer = "";
         String[] allowedSymbols = {"e", ".", "-"};
         int[] ammountOfAllowedSybols = new int[3];
 
@@ -35,121 +144,32 @@ public class StringTasks {
                 strAnswer += str.charAt(i);
             }
         }
-        if (!strAnswer.matches("-?[0-9]+")){
-            if (strAnswer.matches("-?[0-9]+\\.[0-9]+") ||
-                    strAnswer.matches("-?([0-9]+\\.[0-9]+|[0-9]+)e-?[0-9]+")){
-                return Double.valueOf(strAnswer);
-            }
-            return null;
-        }
-        if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
-            return Long.valueOf(strAnswer);
-        }
-        return Integer.valueOf(strAnswer);
-    }
-
-
-    /**
-     * Дополнительно задание
-     * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf() и new Integer и тд
-     */
-    public static Number valueOf(String str) {
-
-        if (str == null || str == "") {
-            return null;
-        }
-        char[] strCopy = new char[str.length()];
-        String strAnswer = "";
-        char[] allowedSymbols = {'e', '.', '-'};
-        int[] ammountOfAllowedSymbols = new int[3];
-        for (int i = 0; i < str.length(); i++) {
-            strCopy[i] = str.charAt(i);
-        }
-        for (int i = 1; i < str.length(); i++) {
-            if (strCopy[i-1] == allowedSymbols[2] && strCopy[i] == allowedSymbols[0]){
-                return null;
-            }
-        }
-        for (char element : strCopy) {
-            for (int i = 0; i < 3; i++) {
-                if (element == allowedSymbols[i]) {
-                    ammountOfAllowedSymbols[i]++;
+        long longAnswer = 0;
+        double doubleAnswer = 0;
+        int intAnswer = 0;
+        if (strAnswer.matches("-?[0-9]+")) {
+            if (strAnswer.matches("-?[0-9]+\\.[0-9]+")) {
+                if (strAnswer.matches("-?([0-9]+\\.[0-9]+|[0-9]+)e-?[0-9]+")) {
+                    return Double.valueOf(strAnswer);
                 }
-            }
-            if (((ammountOfAllowedSymbols[2] > 1) && (ammountOfAllowedSymbols[0] == 0)) ||
-                    ((ammountOfAllowedSymbols[2] > 2) && (ammountOfAllowedSymbols[0] == 1))) {
                 return null;
             }
-            if ((ammountOfAllowedSymbols[2] > 1) && (ammountOfAllowedSymbols[0] == 1) &&
-                    (str.indexOf(allowedSymbols[0]) < str.indexOf(allowedSymbols[2]))) {
-                return null;
-            }
-        }
-        for (int i = 0; i < 2; i++) {
-            if (ammountOfAllowedSymbols[i] > 1) {
-                return null;
-            }
-        }
-        for (int i = 0; i < str.length(); i++) {
 
-            if (!(strCopy[i] >= '0' && strCopy[i] <= '9')) {
-                for (char element : allowedSymbols) {
-                    if (element == strCopy[i]) {
-                        strAnswer += str.charAt(i);
-                    }
-                }
-            } else {
-                str.charAt(i);
-                strAnswer += str.charAt(i);
-            }
-        }
-        if (strAnswer.charAt(strAnswer.length() - 1) == '-') {
             return null;
-        }
-//        if ((ammountOfAllowedSymbols[0] > 0) || (ammountOfAllowedSymbols[1] > 0)) {
-//            return Double.valueOf(strAnswer);
-//        }
-//        if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
-//            return Long.valueOf(strAnswer);
-//        }
-//        return Integer.valueOf(strAnswer);
-
-        //private static getDigit(char str1){ return }
-        long answerLong = 0;
-
-        double answerDouble = 0;
-
-//        if (strAnswer.indexOf('e') != -1) {
-//            String[] strExp = strAnswer.split("e");
-//
-//        }
-        if (strAnswer.charAt(0) == '-'){
-
-            if (strAnswer.indexOf('e') != -1){
-
-
-
-                for (int i = 1; i < strAnswer.length(); i++){
-                    answerLong = (int)Math.pow(10,strAnswer.length()-i) *
-                            Character.digit(strAnswer.charAt(i),10);
-                }
-                answerLong *= -1;
-            }
-        }
-
-
-        for (int i = 0; i < strAnswer.length(); i++){
-            answerLong = (int)Math.pow(10,strAnswer.length()-i) *
-                    Character.digit(strAnswer.charAt(i),10);
-        }
-        if (answerLong < Integer.MAX_VALUE || answerLong > Integer.MIN_VALUE){
-            return (int)answerLong;
         }
         return null;
 
+//        if ((Long.valueOf(strAnswer) > Integer.MAX_VALUE) || (Long.valueOf(strAnswer) < Integer.MIN_VALUE)) {
+//            return Long.valueOf(strAnswer);
+//        }
+
+//        if (strAnswer.indexOf('e') != -1) {
+//            String[] strExp = strAnswer.split("e");
+//        }
     }
 
-   // private static int getDigit(char str1){ return Character.digit(str1, 10); }
+
+
 
 
 }
