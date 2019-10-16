6ba193349c495d5a926a06503c980b3aaa264d13
luckyanya <anya.lakeeva1@mail.ru>
2019-10-16 13:44:52 +0300

    string tasks

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -8,19 +8,50 @@ public class StringTasks {
      * Если '.' и 'e' больше чем 1, возвращаем null
      * Любой класс-обертка над примитивами наследуется от Number
      * Можно использовать функции Double.valueOf() и другие такие же.
-     *
+     * <p>
      * Работайте со строкой, НЕ надо ее переводить в массив байт (это можно использовать только для цикла)
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number simpleValueOf(String str) {
-        return null;
-    }
 
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+
+        String changeStr = str.replaceAll("[^0-9^e//.//-]", "");
+        if (!changeStr.matches("^[-+]?[0-9]*[.,]?[0-9]+(?:[eE][-+]?[0-9]+)?$")) {
+            return null;
+        }
+
+        if (changeStr.contains("e") || changeStr.contains(".")) {
+            return Double.valueOf(changeStr);
+        } else if (Long.valueOf(changeStr) > Integer.MAX_VALUE || Long.valueOf(changeStr) < Integer.MIN_VALUE) {
+            return Long.valueOf(changeStr);
+        } else {
+            return Integer.valueOf(changeStr);
+        }
+    }
 
     /**
      * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf()
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        String changeStr = str.replaceAll("[^0-9^e//.//-]", "");
+        if (!changeStr.matches("^[-+]?[0-9]*[.,]?[0-9]+(?:[eE][-+]?[0-9]+)?$")) {
+            return null;
+        }
+
+        if (changeStr.contains("e") || changeStr.contains(".")) {
+
+            return Double.parseDouble(changeStr);
+        } else if (Long.parseLong(changeStr) > Integer.MAX_VALUE || Long.parseLong(changeStr) < Integer.MIN_VALUE) {
+            return Long.parseLong(changeStr);
+        }
+        return Integer.parseInt(changeStr);
     }
 }
+
+
