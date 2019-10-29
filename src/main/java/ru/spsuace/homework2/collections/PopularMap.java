45e9e5d8e500815039f547496b15581f5d5d415b
MarinaShakhova1946 <shak.marina15@gmail.com>
2019-10-29 21:36:35 +0300

    Задания по коллекциям версия 1.0.

--- src/main/java/ru/spsuace/homework2/collections/PopularMap.java
+++ src/main/java/ru/spsuace/homework2/collections/PopularMap.java
@@ -1,12 +1,10 @@
 package ru.spsuace.homework2.collections;
 
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
 
 /**
  * Написать структуру данных, реализующую интерфейс мапы + набор дополнительных методов.
@@ -14,7 +12,7 @@ import java.util.Set;
  * Популярность - это количество раз, который этот ключ учавствовал в других методах мапы, такие как
  * containsKey, get, put, remove.
  * Считаем, что null я вам не передю ни в качестве ключа, ни в качестве значения
- *
+ * <p>
  * Важный момент, вам не надо реализовывать мапу, вы должны использовать композицию.
  * Вы можете использовать любые коллекции, которые есть в java. Перечислю реализации основных типов коллекций:
  * List -> {@link java.util.ArrayList}
@@ -25,22 +23,25 @@ import java.util.Set;
  * Deque -> {@link java.util.ArrayDeque}
  * Для быстрого перехода в нужный класс или метод, просто зажмите ctrl и щелкните по нему мышкой, или просто щелкните
  * колесиком. Бывает удобно, когда нужно переходить из одной точки кода в другую
- *
+ * <p>
  * Помните, что по мапе тоже можно итерироваться
- *
- *         for (Map.Entry<K, V> entry : map.entrySet()) {
- *             entry.getKey();
- *             entry.getValue();
- *         }
- *
- *
+ * <p>
+ * for (Map.Entry<K, V> entry : map.entrySet()) {
+ * entry.getKey();
+ * entry.getValue();
+ * }
+ * <p>
+ * <p>
  * Дополнительное задание описано будет ниже
+ *
  * @param <K> - тип ключа
  * @param <V> - тип значения
  */
 public class PopularMap<K, V> implements Map<K, V> {
 
     private final Map<K, V> map;
+    private Map<K, Integer> keysMap = new HashMap<>();
+    private Map<V, Integer> valuesMap = new HashMap<>();
 
     public PopularMap() {
         this.map = new HashMap<>();
@@ -52,77 +53,112 @@ public class PopularMap<K, V> implements Map<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return map.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        count(value, valuesMap);
+        return map.containsValue(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        V value = map.get(key);
+        count(key, keysMap);
+        count(value, valuesMap);
+        return value;
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        V valuePut = map.put(key, value);
+        count(value, valuesMap);
+        count(valuePut, valuesMap);
+        count(key, keysMap);
+        return valuePut;
     }
 
     @Override
     public V remove(Object key) {
-        return null;
+        V value = map.remove(key);
+        count(key, keysMap);
+        count(value, valuesMap);
+        return value;
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        throw new UnsupportedOperationException("putAll");
+        map.putAll(m);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return map.values();
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        return map.entrySet();
+    }
+
+    private void count(Object obj, Map map) {
+        if (obj == null) {
+            return;
+        }
+        if (map.containsKey(obj)) {
+            int temp = (int) map.get(obj);
+            temp++;
+            map.put(obj, temp);
+        } else {
+            map.put(obj, 1);
+        }
     }
 
     /**
      * Возвращает самый популярный, на данный момент, ключ
      */
     public K getPopularKey() {
-        return null;
+        return countMostPopular(keysMap);
     }
 
+    public <T> T countMostPopular(Map<T, Integer> map) {
+        T currentPopular = null;
+        int counter = 0;
+        for (Map.Entry<T, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= counter) {
+                currentPopular = entry.getKey();
+                counter = entry.getValue();
+            }
+        }
+        return currentPopular;
+    }
 
     /**
      * Возвращает количество использование ключа
      */
     public int getKeyPopularity(K key) {
-        return 0;
+        return keysMap.get(key);
     }
 
     /**
@@ -130,7 +166,7 @@ public class PopularMap<K, V> implements Map<K, V> {
      * Возвращает самое популярное, на данный момент, значение. Надо учесть что значени может быть более одного
      */
     public V getPopularValue() {
-        return null;
+        return countMostPopular(valuesMap);
     }
 
     /**
@@ -139,6 +175,9 @@ public class PopularMap<K, V> implements Map<K, V> {
      * старое значение и новое - одно и тоже), remove (считаем по старому значению).
      */
     public int getValuePopularity(V value) {
+        if (valuesMap.get(value) != null) {
+            return valuesMap.get(value);
+        }
         return 0;
     }
 
--- src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
+++ src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
@@ -1,17 +1,21 @@
 package ru.spsuace.homework2.collections;
 
+import java.util.HashSet;
 import java.util.Set;
-
 
 /**
  * Реализуйте симметричную разность двух множеств: https://ru.wikipedia.org/wiki/Симметрическая_разность
  * Исходные множества модифицировать нельзя.
  * Пример: {1, 2, 3} и {0, 1, 2} -> {0, 3}.
- *
  */
 public class SymmetricDifference {
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> symmetricDiff = new HashSet<T>(set1);
+        symmetricDiff.addAll(set2);
+        Set<T> tmp = new HashSet<T>(set1);
+        tmp.retainAll(set2);
+        symmetricDiff.removeAll(tmp);
+        return symmetricDiff;
     }
 }
