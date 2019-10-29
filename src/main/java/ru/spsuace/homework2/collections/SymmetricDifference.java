9235149b087074314416a5554b870426084342bf
Анна Устюгова <C:\Users\\AppData\Roaming\The Bat!>
2019-10-29 11:59:10 +0300

    Complete Symmetric Difference task

--- src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
+++ src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
@@ -1,5 +1,6 @@
 package ru.spsuace.homework2.collections;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -7,11 +8,16 @@ import java.util.Set;
  * Реализуйте симметричную разность двух множеств: https://ru.wikipedia.org/wiki/Симметрическая_разность
  * Исходные множества модифицировать нельзя.
  * Пример: {1, 2, 3} и {0, 1, 2} -> {0, 3}.
- *
  */
 public class SymmetricDifference {
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        HashSet<T> collectionResult = new HashSet<>(set2);
+        HashSet<T> collectionRemove = new HashSet<>(set2);
+        collectionResult.addAll(set1);
+        collectionRemove.retainAll(set1);
+        collectionResult.removeAll(collectionRemove);
+
+        return collectionResult;
     }
 }
