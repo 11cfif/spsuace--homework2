fdd4a82dc3ac3f6db1159379f04319770f1e97ba
Dmitry <drujkin.dmitry@yandex.ru>
2019-10-30 13:42:54 +0300

    Symmetric Difference

--- src/main/java/ru/spsuace/homework2/objects/collections/SymmetricDifference.java
+++ src/main/java/ru/spsuace/homework2/objects/collections/SymmetricDifference.java
@@ -1,5 +1,6 @@
-package ru.spsuace.homework2.collections;
+package ru.spsuace.homework2.objects.collections;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -12,6 +13,14 @@ import java.util.Set;
 public class SymmetricDifference {
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> set1Copy = new HashSet<>(set1);
+        Set<T> set2Copy = new HashSet<>(set2);
+
+        set1Copy.removeAll(set2);
+        set2Copy.removeAll(set1);
+
+        Set<T> result = new HashSet<>(set2Copy);
+        result.addAll(set1Copy);
+        return result;
     }
 }
