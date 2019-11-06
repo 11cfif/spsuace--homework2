f3d92bc7f4cc85ac803baec22ade58d36f0f7a2b
16001528 <semen3325@yandex.ru>
2019-11-06 09:22:02 +0300

    PopularMap

--- src/main/java/ru/spsuace/homework2/collections/PopularMap.java
+++ src/main/java/ru/spsuace/homework2/collections/PopularMap.java
@@ -41,6 +41,8 @@ import java.util.Set;
 public class PopularMap<K, V> implements Map<K, V> {
 
     private final Map<K, V> map;
+    private final Map<K, Integer> mapKey = new HashMap<>();
+    private final Map<V, Integer> mapValue = new HashMap<>();
 
     public PopularMap() {
         this.map = new HashMap<>();
@@ -52,69 +54,107 @@ public class PopularMap<K, V> implements Map<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        mapCount(key, mapKey);
+        return map.containsKey(key);
+    }
+
+    private <T> void mapCount(Object obj, Map<T, Integer> map) {
+        if (map.containsKey(obj) && obj != null) {
+            int pattern;
+            pattern = map.get(obj);
+            map.put((T) obj, ++pattern);
+        } else {
+            map.put((T) obj, 1);
+        }
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        mapCount(value, mapValue);
+        return map.containsValue(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        V number = map.get(key);
+        mapCount(key, mapKey);
+        mapCount(number, mapValue);
+
+        return number;
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        V putNumber = map.put(key, value);
+        mapCount(value, mapValue);
+        mapCount(putNumber, mapValue);
+        mapCount(key, mapKey);
+
+        return putNumber;
     }
 
     @Override
     public V remove(Object key) {
-        return null;
+        V removeNumber = map.remove(key);
+        mapCount(key, mapKey);
+        mapCount(removeNumber, mapValue);
+
+        return removeNumber;
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        throw new UnsupportedOperationException("putAll");
+        map.putAll(m);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return map.values();
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        return map.entrySet();
+    }
+
+    public <T> T popularCountMap(Map<T, Integer> map) {
+        T mapPopular = null;
+        int count = 0;
+
+        for (Map.Entry<T, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= count) {
+                mapPopular = entry.getKey();
+                count = entry.getValue();
+            }
+        }
+        return mapPopular;
     }
 
     /**
      * Возвращает самый популярный, на данный момент, ключ
      */
     public K getPopularKey() {
-        return null;
+        return popularCountMap(mapKey);
     }
 
 
@@ -122,7 +162,10 @@ public class PopularMap<K, V> implements Map<K, V> {
      * Возвращает количество использование ключа
      */
     public int getKeyPopularity(K key) {
-        return 0;
+        if (mapKey.get(key) == null){
+            return 0;
+        }
+        return mapKey.get(key);
     }
 
     /**
@@ -130,7 +173,7 @@ public class PopularMap<K, V> implements Map<K, V> {
      * Возвращает самое популярное, на данный момент, значение. Надо учесть что значени может быть более одного
      */
     public V getPopularValue() {
-        return null;
+        return popularCountMap(mapValue);
     }
 
     /**
@@ -139,6 +182,9 @@ public class PopularMap<K, V> implements Map<K, V> {
      * старое значение и новое - одно и тоже), remove (считаем по старому значению).
      */
     public int getValuePopularity(V value) {
+        if (mapValue.get(value) != null) {
+            return mapValue.get(value);
+        }
         return 0;
     }
 
