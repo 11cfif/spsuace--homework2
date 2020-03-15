27a42511b9bd09e1c3dc212d401a52fc65f930d4
UyraIV <UyraIV@mail.ru>
2020-03-15 16:49:41 +0300

    v. 0.0001

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.spsuace.homework2.objects.simple;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -8,10 +10,19 @@ public class MaxTask {
      * Если длина массива меньше count, то вернуть null
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
-     *
      */
-    public static int[] getMaxArray(int[] array, int count) {
-        return null;
-    }
 
+    public static int[] getMaxArray(int[] array, int count) {
+        Arrays.sort(array);
+        if (count > array.length) {
+            return null;
+        }
+        int lastElement = array.length - 1;
+        int[] array1 = new int[count];
+        for (int i = 0; i <= count - 1; i++) {
+            array1[i] = array[lastElement];
+            lastElement -= 1;
+        }
+        return array1;
+    }
 }
