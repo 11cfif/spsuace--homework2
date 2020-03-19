a4d23b01bac3d3f54c1e3b9cca9b6f1929805560
UyraIV <UyraIV@mail.ru>
2020-03-19 19:57:45 +0300

    v. 0.0002 Пока что выполнен только MaxTask

--- src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
@@ -2,8 +2,27 @@ package ru.spsuace.homework2.objects.simple;
 
 /**
  * Опишите иммутабельное комплексное число, с геттерами и методами toString()
- * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
+     * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  * Плюс реализуйте любой метод (операцию) над комплексными числами (на ваш выбор)
  */
 public class ComplexNumber {
+    private final double real;
+    private final double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal(){
+        return real;
+    }
+
+    public double getImaginary(){
+        return imaginary;
+    }
+
+    public double module(){
+        return Math.abs(Math.pow(real, 2) + Math.pow(imaginary, 2));
+    }
 }
