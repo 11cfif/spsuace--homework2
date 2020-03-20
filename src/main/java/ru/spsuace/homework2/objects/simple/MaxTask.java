5587cccd00d9f3707fd5e9958af623bf17a4847c
UyraIV <UyraIV@mail.ru>
2020-03-21 00:51:49 +0300

    v. 0.0004

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -26,4 +26,36 @@ public class MaxTask {
         }
         return array2;
     }
+
+    /**
+     * Вам дан массив и количество элементов в возвращаемом массиве
+     * Вернуть нужно массив из count максимальных элементов array, упорядоченный по убыванию.
+     * Если длина массива меньше count, то вернуть null
+     * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
+     * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
+     * Нельзя пользоваться Arrays.sort
+     */
+
+    public static int[] getMaxArrayHard(int[] array, int count) {
+        int[] arrayI = array;
+        if (count > arrayI.length) {
+            return null;
+        }
+        int tmp;
+        for (int tmp2 = 1; tmp2 < arrayI.length - 1; tmp2++) {
+            for (int i = 1; i < arrayI.length - tmp2; i++)
+                if (array[i] > array[i + 1]) {
+                    tmp = arrayI[i];
+                    arrayI[i] = arrayI[i + 1];
+                    arrayI[i + 1] = tmp;
+                }
+        }
+        int lastElement = arrayI.length - 1;
+        int[] arrayII = new int[count];
+        for (int i = 0; i <= count - 1; i++) {
+            arrayII[i] = arrayI[lastElement];
+            lastElement -= 1;
+        }
+        return arrayII;
+    }
 }
