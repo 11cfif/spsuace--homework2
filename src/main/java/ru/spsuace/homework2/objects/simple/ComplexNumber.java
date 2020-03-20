6c486b28c4a83cd6694253f0224042fee1aff474
raskiri <raskihi13@gmail.com>
2020-03-20 17:22:59 +0300

    all done

--- src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
@@ -1,5 +1,9 @@
 package ru.spsuace.homework2.objects.simple;
 
+import com.sun.javafx.css.Combinator;
+
+import static com.sun.tools.doclint.Entity.and;
+
 /**
  * Опишите иммутабельное комплексное число, с геттерами и методами toString()
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
@@ -7,4 +11,46 @@ package ru.spsuace.homework2.objects.simple;
  * Одна опреация должна быть статической, другая - нет.
  */
 public class ComplexNumber {
+    private int imagine;
+    private int valid;
+
+    public ComplexNumber(int valid, int imagine) {
+        this.imagine = imagine;
+        this.valid = valid;
+    }
+
+    public int getValid() {
+        return valid;
+    }
+
+    public int getImagine() {
+        return imagine;
+    }
+
+    public void setImagine(int imagine) {
+        this.imagine = imagine;
+    }
+
+    public void setValid(int valid) {
+        this.valid = valid;
+    }
+
+    public String toString() {
+        return this.valid + this.imagine + "*i";
+    }
+
+    public boolean equals(ComplexNumber number) {
+        return (this.imagine == number.imagine) & (this.valid == number.valid);
+    }
+    
+    public static ComplexNumber sum(ComplexNumber number1, ComplexNumber number2) {
+        ComplexNumber number = new ComplexNumber(number1.valid+number2.valid,  number1.imagine+number2.imagine);
+        return number;
+    }
+    public ComplexNumber compos(ComplexNumber number1, ComplexNumber number2) {
+        ComplexNumber newNumber = new ComplexNumber(0, 0);
+        newNumber.setValid(number1.getValid() * number2.getValid() - number1.getImagine() * number2.getImagine());
+        newNumber.setImagine(number1.getValid() * number2.getImagine() + number1.getImagine() * number2.getValid());
+        return newNumber;
+    }
 }
