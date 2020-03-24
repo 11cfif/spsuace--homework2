d7239bcf798f49872e75b9732a77c0663839a151
Ярослав <yaroslav.vorobev-2015@mail.ru>
2020-03-24 03:21:50 +0300

    Сделаны 2 задания по массивам.

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -14,7 +14,17 @@ public class MaxTask {
      *
      */
     public static int[] getMaxArraySimple(int[] array, int count) {
-        return null;
+        if (count > array.length) {
+            return null;
+        }
+        Arrays.sort(array);
+        int EndOfArray = array.length - 1;
+        int[] FinishedArray = new int[count];
+        for (int i = 0; i <= count - 1; i++) {
+            FinishedArray[i] = array[EndOfArray];
+            EndOfArray -= 1;
+        }
+        return FinishedArray;
     }
 
     /**
@@ -24,10 +34,26 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * Нельзя пользоваться Arrays.sort
-     *
      */
     public static int[] getMaxArrayHard(int[] array, int count) {
-        return null;
+        if (count > array.length) {
+            return null;
+        }
+        int[] FinishedArray = new int[count];
+        int[] tempArray = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < count; i++) {
+            int max = tempArray[0];
+            for (int k = 0; k < tempArray.length; k++) {
+                max = Math.max(max, tempArray[k]);
+            }
+            FinishedArray[i]=max;
+            for (int j = 0; ; j++) {
+                if (max == tempArray[j]) {
+                    tempArray[j] = Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        return FinishedArray;
     }
-
 }
