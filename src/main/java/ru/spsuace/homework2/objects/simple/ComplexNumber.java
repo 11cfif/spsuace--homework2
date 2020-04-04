8cec9e9414ce59d532e2a5d018961d6b341ff48d
exfena <exfena@mail.ru>
2020-04-04 20:58:00 +0300

    тихонова наталья 2пми1 дз3

--- src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/ComplexNumber.java
@@ -6,26 +6,3 @@ import sun.security.ec.point.ProjectivePoint;
  * Опишите иммутабельное комплексное число, с геттерами и методами toString()
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  * Плюс реализуйте любой метод (операцию) над комплексными числами (на ваш выбор)
- */
-public class ComplexNumber {
-    private int valid;
-    private int imaginary;
-    private String i;
-
-    public ComplexNumber(int valid, int imaginary, String i) {
-        this.valid = valid;
-        this.imaginary = imaginary;
-        this.i = "i";
-    }
-    public void setValid(int valid){
-        this.valid = valid;
-    }
-    public void setImaginary(int imaginary) {
-        this.imaginary = imaginary;
-    }
-    public final void setI(String i) {
-        this.i = "i";
-    }
-
-
-}
