30d20e4f440d0399c79116c35048440be1f3caf2
vitalyaotletaesh <xxxamyakov@mail.ru>
2020-04-28 20:44:15 +0500

    Сделал TextAnalyzer

---
+++ src/main/java/ru/spsuace/homework2/objects/analyzer/TooLongAnalyzer.java
@@ -0,0 +1,19 @@
+package ru.spsuace.homework2.objects.analyzer;
+
+public class TooLongAnalyzer implements TextAnalyzer {
+    private final long maxLengthWord;
+
+    public TooLongAnalyzer(long maxLength) {
+        this.maxLengthWord = maxLength;
+    }
+
+    @Override
+    public FilterType analyze(String text) {
+        FilterType result;
+        result = FilterType.GOOD;
+        if (text.length() > maxLengthWord) {
+            result = FilterType.TOO_LONG;
+        }
+        return result;
+    }
+}
