0618f2acc7b6ba2ce6336238b57ed4521690758d
19000154 <pavlikovan2000@gmail.com>
2020-05-17 14:02:09 +0300

    collection

--- src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
+++ src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
@@ -1,6 +1,7 @@
 package ru.spsuace.homework2.collections;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 /**
  * Реализовать двусвязный список, аналог LinkedList в java (то что я рассказывал на лекции)
@@ -9,54 +10,189 @@ import java.util.Iterator;
  * throw new IndexOutOfBoundsException()
  */
 public class DoubleLinkedList<T> implements Iterable<T> {
+    private int size;
+    private NodeItem<T> >firstItem;
+    private NodeItem<T> >lastItem;
 
     public int size() {
         return 0;
+        return size;
     }
 
     public boolean contains(Object o) {
         return false;
+        public boolean contains (T o){
+            return indexOf(o) != -1;
+        }
+
+        public void clear () {
+
+            size = 0;
+            lastItem = null;
+            firstItem = null;
+        }
+
+        public int indexOf (T o){
+            if (size == 0) {
+                return -1;
+            }
+
+            int i = 0;
+
+            for (NodeItem<T> currentItem = firstItem; currentItem != null; currentItem = currentItem.next, i++) {
+                if (Objects.equals(currentItem.item, o)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void clear () {
+
+        }
+
+        public void add ( int index, T element){
+            if (size < index) {
+                throw new IndexOutOfBoundsException();
+            }
+            if (size == index) {
+                addLast(element);
+            } else {
+                NodeItem<T> currentItem = firstItem;
+                for (int i = 0; i < index; i++) {
+                    currentItem = currentItem.next;
+                }
+                NodeItem<T> newItem = new NodeItem<T>(element, currentItem.prev, currentItem);
+                currentItem.prev = newItem;
+                if (newItem.prev == null) {
+                    firstItem = newItem;
+                } else {
+                    newItem.prev.next = newItem;
+                }
+
+                if (newItem.next == null) {
+                    lastItem = newItem;
+                }
+                size++;
+            }
+        }
+
+        public void addLast (T element){
+            if (lastItem == null) {
+                lastItem = new NodeItem<T>(element, null, null);
+                firstItem = lastItem;
+            } else {
+                NodeItem<T> newItem = new NodeItem<T>(element, lastItem, null);
+                lastItem.next = newItem;
+                lastItem = newItem;
+            }
+            size++;
+        }
+
+
+        public void addFirst (T element){
+
+            if (firstItem == null) {
+                firstItem = new NodeItem<T>(element, null, null);
+                lastItem = firstItem;
+            } else {
+                NodeItem<T> newItem = new NodeItem<T>(element, null, firstItem);
+                firstItem.prev = newItem;
+                firstItem = newItem;
+            }
+            size++;
+        }
+
+
+        public T set ( int index, T element){
+            return null;
+            if (size < index) {
+                throw new IndexOutOfBoundsException();
+            }
+            if (size == index) {
+                addLast(element);
+                return null;
+            } else {
+                NodeItem<T> currentItem = firstItem;
+                for (int i = 0; i < index; i++) {
+                    currentItem = currentItem.next;
+                }
+                NodeItem<T> newItem = new NodeItem<T>(element, currentItem.prev, currentItem.next);
+                T tempItem = currentItem.item;
+                currentItem.item = null;
+                currentItem.next = null;
+                currentItem.prev = null;
+                if (newItem.prev == null) {
+                    firstItem = newItem;
+                } else {
+                    newItem.prev.next = newItem;
+                }
+
+                if (newItem.next == null) {
+                    lastItem = newItem;
+                } else {
+                    newItem.next.prev = newItem;
+                }
+                return tempItem;
+            }
+        }
+
+        public T get ( int index){
+            return null;
+            if (size - 1 < index) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            NodeItem<T> currentItem = firstItem;
+
+            for (int i = 0; i < index; i++) {
+                currentItem = currentItem.next;
+            }
+
+            return currentItem.item;
+        }
+
+        public int indexOf (T o){
+            if (size == 0) {
+                return -1;
+            }
+            int i = 0;
+
+            for (NodeItem<T> currentItem = firstItem; currentItem != null; currentItem = currentItem.next, i++) {
+                if (Objects.equals(currentItem.item, o)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public T remove ( int index){
+            if (currentItem.prev == null) {
+                firstItem = currentItem.next;
+            } else {
+                currentItem.prev.next = currentItem.next;
+            }
+
+            if (currentItem.next == null) {
+                lastItem = currentItem.prev;
+            } else {
+                currentItem.next.prev = currentItem.prev;
+            }
+            T tempItem = currentItem.item;
+            currentItem.next = currentItem.prev;
+            currentItem.prev = currentItem.next;
+            currentItem.item = null;
+            currentItem.next = null;
+            currentItem.prev = null;
+            size--;
+            return tempItem;
+        }
+
+        /**
+         * Дополнительное задание
+         */
+        @Override
+        public Iterator<T> iterator () {
+            return null;
+        }
     }
-
-    public void clear() {
-
-    }
-
-    public void add(int index, T element) {
-
-    }
-
-    public void addLast(T element) {
-
-    }
-
-
-    public void addFirst(T element) {
-
-    }
-
-
-    public T set(int index, T element) {
-        return null;
-    }
-
-    public T get(int index) {
-        return null;
-    }
-
-    public int indexOf(T o) {
-        return -1;
-    }
-
-    public T remove(int index) {
-        return null;
-    }
-
-    /**
-     * Дополнительное задание
-     */
-    @Override
-    public Iterator<T> iterator() {
-        return null;
-    }
-}
