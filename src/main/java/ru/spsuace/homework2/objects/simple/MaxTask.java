c286246d4829028ff4bd52f484394821bfa4ab30
etozhehlus <etozhehlus@gmail.com>
2020-06-01 21:44:55 +0300

    max task + collections

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -11,11 +11,22 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * Можно пользоваться Arrays.sort(arr), эта функция сортирует входящий массив
-     *
      */
     public static int[] getMaxArraySimple(int[] array, int count) {
-        return null;
+        if (count > array.length) {
+            return null;
+        }
+        int[] tempArr = Arrays.copyOf(array, array.length);
+        Arrays.sort(tempArr);
+        int endOfArray = tempArr.length - 1;
+        int[] finalArray = new int[count];
+        for (int i = 0; i < count; i++) {
+            finalArray[i] = tempArr[endOfArray];
+            endOfArray -= 1;
+        }
+        return finalArray;
     }
+
 
     /**
      * Вам дан массив и количество элементов в возвращаемом массиве
@@ -24,10 +35,25 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * Сделать надо без ПОЛНОЙ сортировки массива array
-     *
      */
     public static int[] getMaxArrayHard(int[] array, int count) {
-        return null;
+        if (count > array.length) {
+            return null;
+        }
+        int[] newArray = new int[count];
+        int[] extraArray = Arrays.copyOf(array, array.length);
+        int elementNumb = 0;
+        for (int j = 0; j < count; j++) {
+            int var = Integer.MIN_VALUE;
+            for (int i = 0; i < extraArray.length; i++) {
+                if (extraArray[i] > var) {
+                    var = extraArray[i];
+                    elementNumb = i;
+                }
+            }
+            newArray[j] = var;
+            extraArray[elementNumb] = Integer.MIN_VALUE;
+        }
+        return newArray;
     }
-
 }
